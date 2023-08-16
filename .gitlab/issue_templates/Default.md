14560dc51f04863b159ebec9984bb989b180e2c9
Schmiddiii <gitlab@schmiddi.anonaddy.com>
2023-08-16 14:16:45 +0200

    :construction_worker: Nix build, formatting and GitLab setup

---
+++ .gitlab/issue_templates/Default.md
@@ -0,0 +1,18 @@
+<!-- Before opening an issue, please check a few things:
+
+- Please check if a similar issue already exists.
+- Are YouTube subscriptions not working? This may be the fault of the Piped instance. Please check if setting another instance (in the settings) fixes your issue.
+
+-->
+
+## Description
+
+<!-- Insert description here -->
+
+## Additional information
+
+Installation method: <!-- Probably Flatpak -->
+
+Version: <!-- From the about page -->
+
+Additional information: <!-- Put any remaining information here -->
---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Watch YouTube, LBRY, and PeerTube videos in one place";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          name = "pipeline";
+          legacyname = "tubefeeder";
+          appid = "de.schmidhuberj.tubefeeder";
+        in
+        rec { 
+          packages.default = 
+            with pkgs;
+            stdenv.mkDerivation rec {
+              cargoDeps = rustPlatform.importCargoLock {
+                lockFile = ./Cargo.lock;
+              };
+              src = ./.;
+              buildInputs = with pkgs; [ libadwaita ];
+              nativeBuildInputs = with pkgs; [ wrapGAppsHook4 rustPlatform.cargoSetupHook meson gettext glib pkg-config desktop-file-utils appstream-glib ninja rustc cargo openssl ];
+
+              inherit name;
+            };
+          devShells.default =
+            let 
+              run = pkgs.writeShellScriptBin "run" ''
+                export GSETTINGS_SCHEMA_DIR=./build/data/
+                meson compile -C build && ./build/target/debug/${legacyname}
+              '';
+              check = pkgs.writeShellScriptBin "check" ''
+                cargo clippy
+              '';
+              format = pkgs.writeShellScriptBin "format" ''
+                cargo fmt
+                python3 -m json.tool build-aux/${appid}.json build-aux/${appid}.json
+                xmllint --format --recover data/resources/resources.gresource.xml -o data/resources/resources.gresource.xml
+                xmllint --format --recover data/${appid}.gschema.xml -o data/${appid}.gschema.xml
+                xmllint --format --recover data/${appid}.metainfo.xml -o data/${appid}.metainfo.xml
+                cleancss --format beautify -o data/resources/style.css data/resources/style.css
+              '';
+            in
+            with pkgs;
+            pkgs.mkShell {
+              src = ./.;
+              buildInputs = self.packages.${system}.default.buildInputs;
+              nativeBuildInputs = self.packages.${system}.default.nativeBuildInputs ++ [ rustfmt python3 nodePackages.clean-css-cli libxml2 ] ++ [ run check format ];
+              shellHook = ''
+                meson setup -Dprofile=development build
+              '';
+            };
+          apps.default = {
+            type = "app";
+            inherit name;
+            program = "${self.packages.${system}.default}/bin/${name}";
+          };
+        })
+    );
+}
