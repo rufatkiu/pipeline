200cb1eaabfed3f21aed1353cb9deb12dca02476
Schmiddiii <gitlab@schmiddi.anonaddy.com>
2024-02-22 14:19:53 +0100

    Update nix flake

--- flake.nix
+++ flake.nix
@@ -2,17 +2,13 @@
   description = "Follow video creators";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.nixpkgsgnome.url = "github:NixOS/nixpkgs/704a204ff98d07aec17bd741d304c0ea8c30bc49";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, nixpkgsgnome, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     (flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs {
-            inherit system;
-          };
-          pkgsgnome = import nixpkgsgnome {
             inherit system;
           };
           name = "pipeline";
@@ -37,19 +33,19 @@
                 };
               };
               src = ./.;
-              buildInputs = with pkgs; [ pkgsgnome.libadwaita ];
-              nativeBuildInputs = with pkgs; [ pkgsgnome.wrapGAppsHook4 rustPlatform.cargoSetupHook meson gettext pkgsgnome.glib pkg-config pkgsgnome.desktop-file-utils pkgsgnome.appstream-glib ninja rustc cargo openssl ];
+              buildInputs = with pkgs; [ pkgs.libadwaita ];
+              nativeBuildInputs = with pkgs; [ pkgs.wrapGAppsHook4 rustPlatform.cargoSetupHook meson gettext pkgs.glib pkg-config pkgs.desktop-file-utils pkgs.appstream-glib ninja rustc cargo openssl ];
 
               inherit name;
             };
           devShells.default =
             let 
               run = pkgs.writeShellScriptBin "run" ''
-                export GSETTINGS_SCHEMA_DIR=${pkgs.gtk4}/share/gsettings-schemas/${pkgs.gtk4.name}/glib-2.0/schemas/:${pkgsgnome.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgsgnome.gsettings-desktop-schemas.name}/glib-2.0/schemas/:./build/data/
+                export GSETTINGS_SCHEMA_DIR=${pkgs.gtk4}/share/gsettings-schemas/${pkgs.gtk4.name}/glib-2.0/schemas/:${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas/:./build/data/
                 meson compile -C build && ./build/target/debug/${legacyname}
               '';
               debug = pkgs.writeShellScriptBin "debug" ''
-                export GSETTINGS_SCHEMA_DIR=${pkgs.gtk4}/share/gsettings-schemas/${pkgs.gtk4.name}/glib-2.0/schemas/:${pkgsgnome.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgsgnome.gsettings-desktop-schemas.name}/glib-2.0/schemas/:./build/data/
+                export GSETTINGS_SCHEMA_DIR=${pkgs.gtk4}/share/gsettings-schemas/${pkgs.gtk4.name}/glib-2.0/schemas/:${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas/:./build/data/
                 meson compile -C build && gdb ./build/target/debug/${legacyname}
               '';
               check = pkgs.writeShellScriptBin "check" ''
